5b690f56dd57ab677810faf1483498c1d4cb9448
DTols <mpolotskaia64@gmail.com>
2025-11-15 22:23:16 -0800

    index

--- wasm_demo/src/lib.rs
+++ wasm_demo/src/lib.rs
@@ -1,6 +1,20 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
+pub fn add123(a: i32, b: i32) -> i32 {
     a + b*2
 }
+
+#[wasm_bindgen]
+pub fn addxx(a: i32, b: i32) -> i32 {
+    a + b*2
+}
+
+
+#[wasm_bindgen]
+pub fn modify_bytes(mut data: &mut [u8]) {
+    for b in data.iter_mut() {
+        *b = *b + 1; // modify in place
+    }
+}
+
